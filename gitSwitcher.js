dc8a07db5eaea118c5943aa63bb8a3e111147db3
Shawn <sbaden11@gmail.com>
2016-05-06 15:29:49 -0700

    Pushing master code

---
+++ gitSwitcher.js
@@ -0,0 +1,29 @@
+
+var dayOfTheWeek = 'Tuesday';
+
+switch (dayOfTheWeek){
+	case 'Monday':
+		console.log("It's just another manic Monday");
+		break;
+	case 'Tuesday':
+		console.log('The only day of the week dedicated to a number');
+		break;
+	case 'Wednesday':
+		console.log("They grow up so fast... Pretty soon she'll be Thrusday");
+		break;
+	case 'Thursday':
+		console.log("Friday's speed bump");
+		break;
+	case 'Friday':
+		console.log('My second favorite F word');
+		break;
+	case 'Saturday':
+		console.log('Finally, the weekend');
+		break;
+	case 'Sunday':
+		console.log('Ugh, the weekend is over');
+		break;
+	default:
+		console.log('Invalid day - try again');
+		break;
+}
